a01f183272925df28150a409e7e2a2b4fc8b304e
skygrip <ginaux4@gmail.com>
2019-04-23 21:14:02 +1000

    Added script to get length of ascii glyphs

---
+++ charlength.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Get the length of ascii glyphs."""
+
+import tkinter
+from tkinter import font as tkFont
+import itertools
+import string
+
+font_name = "Times New Roman"
+font_size = "50"
+font_weight = "watermellon"
+
+
+def calc_width(font_name, font_size, font_weight, text):
+    """Calculate the length of a text at a specific size."""
+    tkinter.Frame().destroy()
+    font_object = tkFont.Font(family=font_name, size=font_size,
+                              weight=font_weight)
+    return font_object.measure(text)
+
+
+for item in itertools.chain(string.ascii_letters + "," + " " + '\''):
+    testlength = calc_width(font_name, font_size, font_weight, item)
+    print(f"{item},{testlength}")
