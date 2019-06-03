ec434e9a2c2caaa2249b47e3a68cca2da85e17ac
skygrip <20010424+skygrip@users.noreply.github.com>
2019-06-03 18:34:23 +1000

    Updated script and wordlist

--- un-redactor.py
+++ un-redactor.py
@@ -5,32 +5,42 @@ un-redactor.
 Brute force text that has the same length as redacted words.
 """
 
+import sys
 import tkinter
 from tkinter import font as tkFont
 import itertools
+import time
+
 
 font_name = "Times New Roman"
-font_size = "120"
+font_size = "20"
 font_weight = "normal"
-text = "watermellon"
-
-tkinter.Frame().destroy()
-font_object = tkFont.Font(family=font_name, size=font_size,
-                          weight=font_weight)
+text = sys.argv[1]
 
 
-def calc_width(font_name, font_size, font_weight, text):
-    """Calculate the length of a text at a specific size."""
-    return font_object.measure(text)
+def main():
+    tkinter.Frame().destroy()
+    font_object = tkFont.Font(family=font_name, size=font_size,
+                              weight=font_weight)
+
+    length = font_object.measure(text)
+    print(f"Target length word {text} is {length} long")
+
+    wordList = open("wordlist.txt", "r").readlines()
+    wordList = [item.rstrip() for item in wordList]
+
+    loop_count = 0
+    start_time = time.time()
+
+    for item in itertools.permutations(wordList, 1):
+        testlength = font_object.measure(item)
+        loop_count = loop_count + 1
+        if testlength == length:
+            elapsed_time = time.time() - start_time
+            persecond = loop_count / elapsed_time
+            print(f"answer is {str(item[0])} with length of {testlength}, at loop \
+{loop_count} at rate of {persecond}")
 
 
-length = calc_width(font_name, font_size, font_weight, text)
-print(f"Text {text} is {length} long")
-
-wordList = open("wordlist.txt", "r").readlines()
-wordList = [item.rstrip() for item in wordList]
-
-for item in itertools.permutations(wordList, 1):
-    testlength = calc_width(font_name, font_size, font_weight, item)
-    if testlength == length:
-        print(f"answer is {item} with length of {testlength}")
+if __name__ == "__main__":
+    main()
